0599abe11c0765f069954861f7b10917d602d1a4
jerlin <bruba3010@gmail.com>
2026-03-23 10:04:08 +0530

    Added Docker and docker-compose setup

--- server.js
+++ server.js
@@ -2,33 +2,34 @@ const express = require('express')
 const app = express()
 const connectDb = require('./src/config/db')
 const authRoutes = require('./src/routes/authRoute')
-const protect = require('./src/middleware/authMiddleware')
 const cors = require('cors')
-const taskRoutes = require("./src/routes/taskRoute");
-const errorHandler = require("./src/middleware/errorMiddleware");
+const taskRoutes = require("./src/routes/taskRoute")
+const errorHandler = require("./src/middleware/errorMiddleware")
 
-const swaggerUi = require("swagger-ui-express");
-const swaggerSpec = require("./swagger");
-
+const swaggerUi = require("swagger-ui-express")
+const swaggerSpec = require("./swagger")
 
 const dotenv = require("dotenv")
 dotenv.config()
 
-
 app.use(express.json())
-app.use(cors("*"))
+app.use(cors())
 
 connectDb()
 
+app.use("/api/auth", authRoutes)
+app.use("/api/tasks", taskRoutes)
 
-app.use("/api/auth", authRoutes);
-app.use("/api/tasks", taskRoutes);
+app.get("/", (req, res) => {
+  res.send("Smart Task Manager API is running");
+});
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec))
 
+app.use(errorHandler)
 
-app.listen(process.env.PORT,() => {
-    console.log(`Server is running on port ${process.env.PORT}`)
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
-
-app.use(errorHandler);
